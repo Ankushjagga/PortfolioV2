a837c380c6e8f80704c9bc2691f93cff5d7bc159
ankush jagga <ankush.jagga@mastersunion.org>
2025-10-05 16:01:15 +0530

    fix

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -80,7 +80,7 @@ const Header = () => {
           <li  className = {`headerNav ${activeSection === "contactss" ? "active" : ""}`} onClick={() => {
            handleScroll("contactss")
           }}><i class="fa-solid fa-phone"></i> Contact</li>
-    <NavLink to="/"  className={`headerNav ${activeSection === "resume" ? "active" : ""}`} onClick={handleDownload}><i className="fa-solid fa-download"></i>   Resume</NavLink>
+    <NavLink   className={`headerNav ${activeSection === "resume" ? "active" : ""}`} onClick={handleDownload}><i className="fa-solid fa-download"></i>   Resume</NavLink>
 
     {/* {navigation.map(nav=>{
       console.log(activeSection === nav.to);
